7d2c7822feeda1278bea8ebfe1d94cb69c409275
Aditya Kumar <adityakumarsanni.2001@gmail.com>
2023-06-10 00:37:26 +0530

    optimized upcoming session

--- src/pages/upcoming-sessions/index.js
+++ src/pages/upcoming-sessions/index.js
@@ -5,64 +5,52 @@ import { UpcomingSessionItem } from "@/components/UpcomingSessionItem";
 import { SessionForm } from "@/components/SessionForm";
 import { StudySessionContext } from "@/context/StudySessionContextProvider";
 
-const upComingSessions = () => {
+const UpcomingSessions = () => {
   const sessionCtx = useContext(StudySessionContext);
   const [show, setShow] = useState(false);
   const [sesObj, setSesObj] = useState({});
   const [isLoading, setIsLoading] = useState(false);
-  const [upSessions, setUpSessions] = useState([]);
+  // const [upSessions, setUpSessions] = useState([]);
+  const upSessions = useMemo(
+    () => sessionCtx.upSessions,
+    [sessionCtx.upSessions]
+  );
   useEffect(() => {
     setIsLoading(true);
     sessionCtx.getSessions();
     setIsLoading(false);
   }, []);
-  useEffect(() => {
-    setUpSessions(sessionCtx.upSessions);
-  },[sessionCtx.upSessions])
-  const handleClose = () => {
-    setShow(false);
-  };
-  const updateSessions = (sessionObj) => {
-    setSesObj(sessionObj);
-    setShow(true);
-  };
-  const displaySessions = () => {
+  // useEffect(() => {
+  //   setUpSessions(sessionCtx.upSessions);
+  // }, [sessionCtx.upSessions]);
+  const updateSessions = useMemo(
+    (sessionObj) => {
+      setSesObj(sessionObj);
+      setShow(true);
+    },
+    [sessionObj]
+  );
+  const handleSort = () => {
     const sortedUpSessions = [...upSessions];
     const propComp = (sD, eD, sT, eT) => (a, b) => {
       if (a[sD] !== b[sD]) {
-        if (a[sD] > b[sD]) {
-          return 1;
-        } else {
-          return -1;
-        }
+        return a[sD] > b[sD] ? 1 : -1;
       } else if (a[eD] !== b[eD]) {
-        if (a[eD] > b[eD]) {
-          return 1;
-        } else {
-          return -1;
-        }
+        return a[eD] > b[eD] ? 1 : -1;
       } else if (a[sT] !== b[sT]) {
-        if (a[sT] > b[sT]) {
-          return 1;
-        } else {
-          return -1;
-        }
+        return a[sT] > b[sT] ? 1 : -1;
       } else if (a[eT] !== b[eT]) {
-        if (a[eT] > b[eT]) {
-          return 1;
-        } else {
-          return -1;
-        }
+        return a[eT] > b[eT] ? 1 : -1;
       } else {
         return 0;
       }
     };
-    sortedUpSessions.sort(propComp("startDate", "endDate", "startTime", "endTime"));
-    setUpSessions(sortedUpSessions);
+    sortedUpSessions.sort(
+      propComp("startDate", "endDate", "startTime", "endTime")
+    );
+    sessionCtx.setUpSessions(sortedUpSessions);
   };
-  const handleSort = () => {
-    displaySessions();
-  };
+
   let modal = (
     <Modal show={show} onHide={handleClose}>
       <Modal.Header closeButton />
@@ -71,7 +59,7 @@ const upComingSessions = () => {
           method="PUT"
           isModal={true}
           sessionObj={sesObj}
-          handleClose={handleClose}
+          handleClose={() => setShow(false)}
           title={"Edit the Session"}
         />
       </Modal.Body>
@@ -81,7 +69,7 @@ const upComingSessions = () => {
   let content = null;
   if (isLoading) {
     content = <LoadingSpinner />;
-  } else if (upSessions?.length === 0|| upSessions===undefined) {
+  } else if (!upSessions || upSessions.length === 0) {
     content = (
       <div style={{ margin: "auto", fontSize: "20px" }}>
         No, upcoming Study sessions!
@@ -124,4 +112,4 @@ const upComingSessions = () => {
   );
 };
 
-export default upComingSessions;
+export default UpcomingSessions;
