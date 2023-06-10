927d41ea58963e3020072be9f8e616489a09b4fd
Aditya Kumar <75529750+Aditya61101@users.noreply.github.com>
2023-06-10 13:04:29 +0530

    Update sessions.js

--- src/pages/api/sessions.js
+++ src/pages/api/sessions.js
@@ -2,13 +2,18 @@ import UpcomingSessions from "@/models/upcomingSessions";
 import connectDB from "@/config/db";
 
 const handler = async (_, res) => {
-  const upComingSessions = await UpcomingSessions.find();
-  if (upComingSessions?.length > 0) {
-    res.status(200).json({ success: true, upComingSessions });
-  } else {
-    res
-      .status(200)
-      .json({ success: false, error: "No upcoming sessions found!" });
+  try{
+      const upComingSessions = await UpcomingSessions.find();
+    if (upComingSessions?.length > 0) {
+      res.status(200).json({ success: true, upComingSessions });
+    } else {
+      res
+        .status(200)
+        .json({ success: false, error: "No upcoming sessions found!" });
+    }
+  } catch {
+     res.status(500).json({ success: false, error: "Internal Server Error" });
   }
+  
 };
 export default connectDB(handler);
