50323cd376a9b0b605b11306c8be56c0c1b12ddd
pooranjoyb <pooranjoyb2016@gmail.com>
2023-12-09 23:53:17 +0530

    Updated Model WRT new Data

--- src/models/user.js
+++ src/models/user.js
@@ -1,6 +1,7 @@
 import mongoose from "mongoose";
 
-const userSchema = mongoose.Schema({
+const userSchema = mongoose.Schema(
+  {
     email: {
       type: String,
       required: [true, "Please enter a valid email address!"],
@@ -10,7 +11,24 @@ const userSchema = mongoose.Schema({
       type: String,
       required: [true, "Password should be of at least 6 characters!"],
     },
-  },{
+    name: {
+      type: String,
+      required: [true, "Obviously you have a name!"],
+    }, 
+    age: {
+      type: Number,
+      required: true
+    },
+    college: {
+      type: String,
+      required: true
+    },
+    address: {
+      type: String,
+      required: true
+    }
+  },
+  {
     timestamps: true,
   }
 );
